f17a5df2b7eda933671e2334e74be7bc2596f172
Emmanuel-Deko <emmanueldeko64@gmail.com>
2023-07-27 11:41:13 +0100

    Initialisations des messages qui devaient etre generés

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // Bienvenue dans notre projet : Qui parle de la politique
 
 function genererMessageAleatoire() {
-   
-    "La grammaire française est basée sur des règles précises."
-    
-    "Les verbes français sont souvent conjugués selon le temps et le mode."
 
-    
+    // Initialisation des valeurs
+
+    const messages = [
+        "La grammaire française est basée sur des règles précises.",
+      "Les verbes français sont souvent conjugués selon le temps et le mode.",
+      "Les pronoms personnels en français sont : je, tu, il/elle, nous, vous, ils/elles.",
+      "Les articles définis en français sont : le, la, les.",
+    ];
   }
-  
   genererMessageAleatoire();
