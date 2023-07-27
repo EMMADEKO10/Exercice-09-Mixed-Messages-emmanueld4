88fc6f5c8bda2f558a6f2b88f8634879377c7511
Emmanuel-Deko <emmanueldeko64@gmail.com>
2023-07-27 12:24:22 +0100

    Fin du programme

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 // Bienvenue dans notre projet : Qui parle du francais Grammaire
 
 function genererMessageAleatoire() {
-
     // Initialisation des valeurs
-
     const messages = [
         "La grammaire française est basée sur des règles précises.",
-      "Les verbes français sont souvent conjugués selon le temps et le mode.",
-      "Les pronoms personnels en français sont : je, tu, il/elle, nous, vous, ils/elles.",
-      "Les articles définis en français sont : le, la, les.",
-      "Les articles indéfinis en français sont : un, une, des.",
-      "Les adjectifs en français s'accordent en genre et en nombre avec le nom qu'ils qualifient.",
-      "La négation en français se forme en utilisant le mot 'ne' suivi d'un autre mot négatif.",
-      "Les prépositions en français indiquent la relation entre deux mots dans une phrase."
-
+        "Les verbes français sont souvent conjugués selon le temps et le mode.",
+        "Les pronoms personnels en français sont : je, tu, il/elle, nous, vous, ils/elles.",
+        "Les articles définis en français sont : le, la, les.",
+        "Les articles indéfinis en français sont : un, une, des.",
+        "Les adjectifs en français s'accordent en genre et en nombre avec le nom qu'ils qualifient.",
+        "La négation en français se forme en utilisant le mot 'ne' suivi d'un autre mot négatif.",
+        "Les prépositions en français indiquent la relation entre deux mots dans une phrase."
     ];
-    //Generer le messqge
 
-  }
-  const messageAleatoire = messages[Math.floor(Math.random() * messages.length)];
-  genererMessageAleatoire();
+    // Générer le message
+    const messageAleatoire = messages[Math.floor(Math.random() * messages.length)];
+    console.log(messageAleatoire);
+}
+
+genererMessageAleatoire();
