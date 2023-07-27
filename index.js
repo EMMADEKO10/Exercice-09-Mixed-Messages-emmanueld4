0c85747b85bb2f9f64e7554bfe0488cbbd9dbbb9
Emmanuel-Deko <emmanueldeko64@gmail.com>
2023-07-27 12:02:49 +0100

    Fin initialisation des messages

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Bienvenue dans notre projet : Qui parle de la politique
+// Bienvenue dans notre projet : Qui parle du francais Grammaire
 
 function genererMessageAleatoire() {
 
@@ -9,6 +9,11 @@ function genererMessageAleatoire() {
       "Les verbes français sont souvent conjugués selon le temps et le mode.",
       "Les pronoms personnels en français sont : je, tu, il/elle, nous, vous, ils/elles.",
       "Les articles définis en français sont : le, la, les.",
+      "Les articles indéfinis en français sont : un, une, des.",
+      "Les adjectifs en français s'accordent en genre et en nombre avec le nom qu'ils qualifient.",
+      "La négation en français se forme en utilisant le mot 'ne' suivi d'un autre mot négatif.",
+      "Les prépositions en français indiquent la relation entre deux mots dans une phrase."
+
     ];
   }
   genererMessageAleatoire();
